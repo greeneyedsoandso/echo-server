5f978c07801ce5a9409e678602539a80da26f586
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-10-04 16:42:18 -0700

    Added correct return, dithered on REUSEADDR vs REUSEPORT. Each test passes individually, but not when run as a suite.

--- echo_server.py
+++ echo_server.py
@@ -7,7 +7,7 @@ def server(log_buffer=sys.stderr):
     # set an address for our server
     address = ('127.0.0.1', 10000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     # log that we are building a server
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
     sock.bind(address)
@@ -26,25 +26,22 @@ def server(log_buffer=sys.stderr):
                 # buffers.  When a complete message has been received, the
                 # loop will exit
                 while True:
-                    buffer = 16
-                    data = conn.recv(buffer)
+                    data = conn.recv(16)
                     print('received "{0}"'.format(data.decode('utf8')))
                     conn.sendall(data)
                     print('sent "{0}"'.format(data.decode('utf8')))
-                    if data == b'':
-                        break
+                    if len(data) < 16:
+                        return False
 
             except Exception as e:
                 traceback.print_exc()
                 sys.exit(1)
             finally:
-                # TODO: When the inner loop exits, this 'finally' clause will
-                #       be hit. Use that opportunity to close the socket you
-                #       created above when a client connected.
+                sock.close()
                 print(
                     'echo complete, client connection closed', file=log_buffer
                 )
-                sock.close()
+
     except KeyboardInterrupt:
         print('quitting echo server', file=log_buffer)
         sock.close()
